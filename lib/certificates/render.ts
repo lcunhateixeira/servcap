c44e926d7a709d1585b5c3412bb62c39730e02f2
lcunhateixeira <lucianocunhateixeira@outlook.com>
2025-12-31 16:21:23 -0300

    teste label

--- lib/certificates/render.ts
+++ lib/certificates/render.ts
@@ -6,6 +6,7 @@ import { PDFDocument } from "pdf-lib";
 import { createAdminClient } from "@/lib/supabase/admin";
 import { formatDateBR } from "@/lib/formatters/date";
 import { wrapSvgText } from "../svg/wrapText";
+import { Resvg } from "@resvg/resvg-js";
 
 function escapeXml(s: string) {
   return (s ?? "")
@@ -172,7 +173,7 @@ export async function renderCertificateFiles(certId: string) {
   //   <image href="${qrDataUrl}" x="${Math.round(W * 0.58)}" y="${Math.round(H * 0.015)}"
   //     width="${Math.round(W * 0.08)}" height="${Math.round(W * 0.08)}" />
   // </svg>`;
-   const overlaySvg = `
+  const overlaySvg = `
   <svg xmlns="http://www.w3.org/2000/svg" width="${W}" height="${H}">
     <defs>
       <style type="text/css">
@@ -228,8 +229,15 @@ export async function renderCertificateFiles(certId: string) {
 
 
   // 9) compor PNG final
+  // renderiza o SVG em PNG com fontes funcionando
+  const resvg = new Resvg(overlaySvg, {
+    fitTo: { mode: "width", value: W },
+  });
+  const overlayPng = resvg.render().asPng();
+
+  // agora sim compõe com o template
   const pngBuffer = await sharp(templateBuffer)
-    .composite([{ input: Buffer.from(overlaySvg), top: 0, left: 0 }])
+    .composite([{ input: overlayPng, top: 0, left: 0 }])
     .png()
     .toBuffer();
 
